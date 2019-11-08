6f3a0f11a7f12b63d6ddcfe1fa90d74b14fce5ae
root <root@LAPTOP-UL2RC4PT.localdomain>
2019-11-08 16:56:40 -0600

    Implements insertion and traversal functions in BST class

--- src/bst.hpp
+++ src/bst.hpp
@@ -3,6 +3,7 @@
 // include this library to use NULL, otherwise use nullptr instead
 #include <cstddef>
 #include <iostream>
+#include <vector>
 #include "node.hpp"
 
 template<class T>
@@ -36,6 +37,14 @@ public:
     // returns: the number of nodes in the tree
     int get_size(void);
 private:
+    // helper function for insert(T)
+    Node<T>* insertHelper(T data, Node<T>* node);
+    // helper function for inorder traversal
+    void inorderHelper(std::vector<T>* vec, Node<T>* node);
+    // helper function for preorder traversal
+    void preorderHelper(std::vector<T>* vec, Node<T>* node);
+    // helper function for postorder traversal
+    void postorderHelper(std::vector<T>* vec, Node<T>* node);
     // the root node of the tree
     Node<T> *root;
     // the number of nodes in the tree
@@ -59,35 +68,101 @@ BST<T>::~BST()
     }
 }
 
+// Traversal functions handle the recursion and also adding node data to the vector.
+template<class T>
+void BST<T>::inorderHelper(std::vector<T>* vec, Node<T>* node)
+{
+    if(node == NULL)
+    {
+        return;
+    }
+
+    inorderHelper(vec, node->get_left());
+    vec->push_back(node->get_data());
+    inorderHelper(vec, node->get_right());
+}
+
 template<class T>
  std::vector<T> * BST<T>::inorder()
 {
     std::vector<T> *vec = new std::vector<T>;
+    inorderHelper(vec, root);
 
     return vec;
 }
 
+// Traversal functions handle the recursion and also adding node data to the vector.
+template<class T>
+void BST<T>::preorderHelper(std::vector<T>* vec, Node<T>* node)
+{
+    if(node == NULL)
+    {
+        return;
+    }
+
+    vec->push_back(node->get_data());
+    preorderHelper(vec, node->get_left());
+    preorderHelper(vec, node->get_right());
+}
 
 template<class T>
  std::vector<T> * BST<T>::preorder()
 {
     std::vector<T> *vec = new std::vector<T>;
+    preorderHelper(vec, root);
+
     return vec;
 }
 
+// Traversal functions handle the recursion and also adding node data to the vector.
+template<class T>
+void BST<T>::postorderHelper(std::vector<T>* vec, Node<T>* node)
+{
+    if(node == NULL)
+    {
+        return;
+    }
+
+    postorderHelper(vec, node->get_left());
+    postorderHelper(vec, node->get_right());
+    vec->push_back(node->get_data());
+}
 
 template<class T>
  std::vector<T> * BST<T>::postorder()
 {
     std::vector<T> *vec = new std::vector<T>;
+    postorderHelper(vec, root);
 
     return vec;
 }
 
 template<class T>
+Node<T>* BST<T>::insertHelper(T data, Node<T>* node)
+{
+    if(node == NULL)
+    {
+        Node<T>* tmp = new Node<T>;
+        tmp->set_data(data);
+        tmp->set_left(NULL);
+        tmp->set_right(NULL);
+
+        return tmp;
+    }
+    else if(data < node->get_data())
+    {
+        node->set_left(insertHelper(data, node->get_left()));
+    }
+    else if(data > node->get_data())
+    {
+        node->set_right(insertHelper(data, node->get_right()));
+    }
+}
+
+template<class T>
 void BST<T>::insert(T new_data)
 {
-
+    root = insertHelper(new_data, root);
 }
 
 
